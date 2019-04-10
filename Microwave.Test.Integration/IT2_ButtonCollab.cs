952e847f83ef85237c804f1f6ab4fa9f3627bbbf
Laurits Dyrberg Hansen <201706994@post.au.dk>
2019-04-10 10:04:36 +0200

    Lavet driver til IT2

--- Microwave.Test.Integration/IT2_ButtonCollab.cs
+++ Microwave.Test.Integration/IT2_ButtonCollab.cs
@@ -1,7 +1,19 @@
-﻿namespace Microwave.Test.Integration
+﻿using System.Runtime.InteropServices;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace Microwave.Test.Integration
 {
+   
     public class IT2_ButtonCollab
     {
-        
+        private IButton powerButton;
+        private IButton timeButton;
+        private IButton startCanceButton;
+        private IDoor door;
+         
+
+
+
     }
 }
