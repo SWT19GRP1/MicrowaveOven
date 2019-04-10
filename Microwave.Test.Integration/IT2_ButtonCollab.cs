5db8762aceb929360804d5f2fc19100aace93f45
Laurits Dyrberg Hansen <201706994@post.au.dk>
2019-04-10 10:12:30 +0200

    added Setup for IT2

--- Microwave.Test.Integration/IT2_ButtonCollab.cs
+++ Microwave.Test.Integration/IT2_ButtonCollab.cs
@@ -1,16 +1,29 @@
 ﻿using System.Runtime.InteropServices;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
-   
+   [TestFixture]
     public class IT2_ButtonCollab
     {
-        private IButton powerButton;
-        private IButton timeButton;
-        private IButton startCanceButton;
-        private IDoor door;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _door;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _timeButton = new Button();
+            _door = new Door();
+        
+  
+
+        }
          
 
 
